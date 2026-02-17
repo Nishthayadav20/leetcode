d62bf081b9480b2e5cb6b6b66ca7d5e1aa571c86
Nishtha2024 <nishtha2327.cs1051@kiet.edu>
2026-02-18 01:02:35 +0530

    first commit

---
+++ repeated substring pattern.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        
+        for(int len = 1; len <= n/2; len++) {
+            if(n % len == 0) {
+                String sub = s.substring(0, len);
+                String formed = "";
+                
+                for(int i = 0; i < n/len; i++) {
+                    formed += sub;
+                }
+                
+                if(formed.equals(s)) return true;
+            }
+        }
+        return false;
+    }
+}
